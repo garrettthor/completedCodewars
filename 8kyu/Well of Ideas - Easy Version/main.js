3b80947a923761a93e6c6ba9c77dcfa3d9a7a9de
Garrett Thor <garrettthor@gmail.com>
2021-04-09 13:27:38 -0700

    well of ideas

---
+++ 8kyu/Well of Ideas - Easy Version/main.js
@@ -0,0 +1,28 @@
+/*
+For every good kata idea there seem to be quite a few bad ones!
+
+In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+*/
+
+
+function well(x){
+    let goodCount = 0
+    for (i=0; i<x.length; i++){
+        if (x[i] === 'good'){
+            goodCount +=1
+        }
+    }
+    if (goodCount > 0 && goodCount < 3){
+        return 'Publish!'
+    } else if (goodCount > 2){
+        return 'I smell a series!'
+    } else {
+        return 'Fail!'
+    }
+}
+
+
+well(['bad', 'bad', 'bad'])
+well(['good', 'bad', 'bad', 'bad', 'bad'])
+well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good'])
